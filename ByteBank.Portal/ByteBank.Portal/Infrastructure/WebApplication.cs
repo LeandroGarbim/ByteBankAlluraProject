a1ab78fb1bd716bcf774c6a0598cac0e9c0692d1
leandro.lopes <leandro.lopes@premmiar.com.br>
2022-05-07 15:25:56 -0300

    first commit

---
+++ ByteBank.Portal/ByteBank.Portal/Infrastructure/WebApplication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infrastructure
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixes;
+        public WebApplication(string [] prefixes)
+        {
+            if(prefixes == null)
+            {
+                throw new ArgumentNullException(nameof(prefixes));
+                _prefixes = prefixes;
+            }
+        }
+        public void Iniciar()
+        {
+            var httpListener = new HttpListener();
+
+            foreach(prefixe in _prefixes)
+                httpListener.Prefixes.Add(prefixe);           
+
+            httpListener.Start();
+
+            var context = httpListener.GetContext();
+            var request = context.Request;
+            var response = context.Response;
+
+
+        }
+    }
+}
